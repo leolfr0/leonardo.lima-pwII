2f943906f16abe78da5469d975bb53c6d6dc1a9f
leonardo <leonardolimadefreitas09@gmail.com>
2026-04-14 08:33:50 -0300

    Aulas de PWII feitas no dia 14/04/2026

---
+++ aula 14_04/js/nome.js
@@ -0,0 +1,7 @@
+var nome="Leonardo";
+var nome2=prompt("Digite seu nome: ");
+var idade=parseInt(prompt("Digite sua idade: "));
+
+console.log("Seu nome é: ",nome);
+alert("Seu nome é: "+nome2);
+alert("Sua idade é: "+idade);
---
+++ aula 14_04/js/soma.js
@@ -0,0 +1,4 @@
+var numero1=parseFloat(prompt("Digite o primeiro número: "));
+var numero2=parseFloat(prompt("Digite o segundo número: "));
+
+console.log("A soma é: ",numero1+numero2);
