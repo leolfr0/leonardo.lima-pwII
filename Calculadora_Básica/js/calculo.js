9ff7e086c0c7a2976f63478f316b9615a32cb11f
Leonardo Lima <leonardolimadefreitas09@gmail.com>
2026-04-29 17:48:16 -0300

    Calculadora básica, feita com HTML e JS. Finalizada no dia 29/04/2026

--- Calculadora_Básica/js/calculo.js
+++ Calculadora_Básica/js/calculo.js
@@ -1,1 +1,24 @@
-var n1 
+var n1 = document.querySelector("#n1");
+var n2 = document.querySelector("#n2");
+var resultado = document.querySelector("#resultado");
+
+function somar() {
+    resultado.innerHTML = Number(n1.value)+Number(n2.value);
+}
+
+function subtrair() {
+    resultado.innerHTML=Number(n1.value)-Number(n2.value);
+}
+
+function multiplicar() {
+    resultado.innerHTML=Number(n1.value)*Number(n2.value);
+}
+
+function dividir() {
+    if(Number(n2.value)==0){
+        resultado.innerHTML="Erro: divisão por 0";
+    }
+    else{
+        resultado.innerHTML=Number(n1.value)/Number(n2.value);
+    }
+}
