141a5e467d97616f5f6b71868d341b908979d646
leonardo <leonardolimadefreitas09@gmail.com>
2026-05-12 08:37:35 -0300

    Atualizações na calculadora de acordo com as orientações da aula do dia 12/05/2026

--- Calculadora_Básica/js/calculo.js
+++ Calculadora_Básica/js/calculo.js
@@ -1,6 +1,7 @@
 var n1 = document.querySelector("#n1");
 var n2 = document.querySelector("#n2");
 var resultado = document.querySelector("#resultado");
+var select = document.getElementById("opcoes");
 
 function somar() {
     resultado.innerHTML = Number(n1.value)+Number(n2.value);
@@ -23,30 +24,58 @@ function dividir() {
     }
 }
 
-function AreaTriangulo(){
-    if(n1.value==0 || n2.value==0){
+function areaTriangulo(){
+    if(Number(n1.value)==0 || Number(n2.value)==0){
         resultado.innerHTML="Erro: não é possível fazer área por 0";
     }
     else{
-        resultado.innerHTML=(n1.value*n2.value)/2;
+        resultado.innerHTML=(Number(n1.value)*Number(n2.value))/2;
     }
 }
 
-function AreaQuadrado(){
-    if (n1.value==0){
+function areaQuadrado(){
+    if (Number(n1.value)==0){
         resultado.innerHTML="Erro: não é possível fazer área por 0";
     }
     else{
-        resultado.innerHTML=n1.value*n1.value;
+        resultado.innerHTML=Number(n1.value)*Number(n1.value);
     }
 }
 
-function ParImpar(){
-    if (n1.value%2==0){
+function parImpar(){
+    if (Number(n1.value)%2==0){
         resultado.innerHTML="O número é par";
     }
     else{
         resultado.innerHTML="O número é ímpar";
+    }
+}
+
+function menos10Porcento(){
+    n1.value-=Number(n1.value)*0.1
+    resultado.innerHTML=n1.value;
+}
+
+function menos3Porcento(){
+    resultado.innerHTML=n1.value-=Number(n1.value)*0.03;
+}
+
+function lucro15Porcento(){
+    n1.value=Number(n1.value)+(Number(n1.value)*0.15);
+    resultado.innerHTML=n1.value;
+}
+
+function venda(){
+    if(Number(n1.value)>1000){
+        n1.value-=Number(n1.value*0.07);
+        resultado.innerHTML=n1.value;
+    }
+    else if(Number(n1.value)<200){
+        resultado.innerHTML="Boa compra!";
+    }
+    else if(Number(n1.value)<=500){
+        n1.value=Number(n1.value)+(Number(n1.value)*0.05);
+        resultado.innerHTML=n1.value;
     }
 }
 
